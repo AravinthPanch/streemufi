52fd2da76c9572c243df8e580367f408651de044
ligi <ligi@ligi.de>
2013-10-05 20:07:49 +0200

    copy UI to a artist singleton for later save

---
+++ android/src/main/java/org/hack4good/streemufi/App.java
@@ -0,0 +1,9 @@
+package org.hack4good.streemufi;
+
+import android.app.Application;
+
+import org.hack4good.streemufi.model.Artist;
+
+public class App extends Application {
+    public static Artist actArtist = new Artist();
+}
--- android/src/main/java/org/hack4good/streemufi/EditDataActivity.java
+++ android/src/main/java/org/hack4good/streemufi/EditDataActivity.java
@@ -1,12 +1,15 @@
 package org.hack4good.streemufi;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+
+import org.hack4good.streemufi.model.Artist;
 
 public class EditDataActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +20,25 @@ public class EditDataActivity extends Activity {
         findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                copyUI2Artist();
+
                 Intent intent = new Intent(EditDataActivity.this, UploadVideoActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    private void copyUI2Artist() {
+        Artist artist = App.actArtist;
+
+        EditText name = (EditText) findViewById(R.id.name);
+        EditText contact = (EditText) findViewById(R.id.contact);
+        EditText text = (EditText) findViewById(R.id.notes);
+
+        artist.name = name.toString();
+        artist.contact = contact.toString();
+        artist.text = text.toString();
     }
 
 }
