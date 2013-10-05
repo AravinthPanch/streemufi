e2ed77921bddb4cf2d99f04971869276c1f05451
ligi <ligi@ligi.de>
2013-10-05 14:07:54 +0200

    refactor and add submit button

--- android/src/main/java/org/hack4good/streemufi/EditDataActivity.java
+++ android/src/main/java/org/hack4good/streemufi/EditDataActivity.java
@@ -9,7 +9,7 @@ public class EditDataActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_edit_data);
         setTitle("Step 3 Artist-Data");
     }
 
