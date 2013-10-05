d41870f45b52113b0b5e7ff8d397f831501419bd
Nikolas Martesn <nikolas.m@rtens.org>
2013-10-05 13:20:48 +0200

    Moved RendererFactory initialisation to WebApplication

--- web/src/streemufi/web/artist/ListComponent.php
+++ web/src/streemufi/web/artist/ListComponent.php
@@ -2,19 +2,10 @@
 namespace streemufi\web\artist;
 
 use watoki\curir\controller\Component;
-use watoki\curir\controller\Module;
-use watoki\curir\Path;
-use watoki\factory\Factory;
-use watoki\tempan\Renderer;
 
 class ListComponent extends Component {
 
     static $CLASS = __CLASS__;
-
-    function __construct(Factory $factory, Path $route, Module $parent = null) {
-        parent::__construct($factory, $route, $parent);
-        $this->rendererFactory->setRenderer('html', Renderer::$CLASS);
-    }
 
     public function doGet() {
         return array();
