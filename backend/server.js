19c8e172a16b3764cff8bb7ccf24474d2c966de8
Aravinth <aravinth.panchadcharam@yetu.com>
2013-10-05 12:40:07 +0200

    Node Setup

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin' : '*'}); 	  
   res.end('Connected to NodeJS\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(61050, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:61050/');
