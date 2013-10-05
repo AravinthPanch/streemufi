bc962f82cb9336d4f7aede8382be14a6e09f70bf
Aravinth <aravinth.panchadcharam@yetu.com>
2013-10-05 13:37:39 +0200

    Node Setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,26 @@
+var results
+
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin' : '*'}); 	  
-  res.end('Connected to NodeJS\n');
-}).listen(61050, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:61050/');
+  res.writeHead(200, {'Content-Type': 'text/plain' }); 	     
+  res.end('Connected to NodeJS\n' + db());
+}).listen(61555, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:61555/');
+
+function db() {
+var mysql      = require('mysql');
+var connection = mysql.createConnection({ 
+  user     : 'localhost',
+  password : 'yivGealebeagg'   
+});
+
+connection.connect(function(err) {
+	console.log('connected')
+});
+
+connection.query('select * from user', function(err, result) {  	
+	results = result[0].username
+});
+return results;
+connection.end();
+}
