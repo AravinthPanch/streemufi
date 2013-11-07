24a1045a68b70c4dca591cf65eb808b9f5c65609
martens <nikolas.m@rtens.org>
2013-11-07 10:14:33 +0100

    Updated curir

--- web/src/streemufi/web/streemufi/ArtistsResource.php
+++ web/src/streemufi/web/streemufi/ArtistsResource.php
@@ -1,7 +1,6 @@
 <?php
 namespace streemufi\web\streemufi;
 
-use streemufi\Configuration;
 use streemufi\stores\ArtistStore;
 use streemufi\web\Presenter;
 use streemufi\web\StreemufiResource;
@@ -14,9 +13,6 @@ class ArtistsResource extends Container {
     /** @var ArtistStore <- */
     private $store;
 
-    /** @var Configuration <- */
-    private $config;
-
     public function doGet() {
         return new Presenter(array(
             'artist' => $this->assembleArtists()
@@ -26,14 +22,10 @@ class ArtistsResource extends Container {
     private function assembleArtists() {
         $artists = array();
         foreach ($this->store->readAll() as $artist) {
-            $url = $this->getAncestor(StreemufiResource::$CLASS)->getUrl()->copy();
-            $url->getPath()->append('artist');
-            $url->getPath()->append($artist['key']);
-
             $artists[] = array(
                 'name' => $artist['name'],
                 'url' => array(
-                    'href' => $url->toString()
+                    'href' => $this->getAncestor(StreemufiResource::$CLASS)->getUrl('artist/' . $artist['key'])->toString()
                 ),
                 'location' => null
             );
